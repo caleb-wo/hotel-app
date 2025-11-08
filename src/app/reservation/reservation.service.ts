5726a6fbb30f84ee7fe32cf99634660074502fc5
calebwolfe <caleb-wolfe@gmx.com>
2025-11-08 14:17:30 -0700

    build routing, reservation type, service, and /new form+formGroup.

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
 
 @Injectable({
   providedIn: 'root'
-})
+}) 
 export class ReservationService {
+  private reservations: Reservation[] = []
 
-  constructor() { }
+  //CRUD
+  getReservations(): Reservation[]{
+    return this.reservations;
+  }
+
+  getReservation(id: string):Reservation|undefined {
+    return this.reservations.find(res=> res.id === id);
+  }
+
+  addReservation(reservation: Reservation):void {
+    this.reservations.push(reservation);
+  }
+
+  deleteReservation(id: string):void {
+    let index = this.reservations.findIndex(res=> res.id === id);
+    this.reservations.splice(index, 1);
+  }
+
+  updateReservation(updatedRes: Reservation):void {
+    let index = this.reservations.findIndex(res=> res.id === updatedRes.id);
+    this.reservations[index] = updatedRes;
+  }
 }
